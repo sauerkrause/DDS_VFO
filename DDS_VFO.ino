1a787385eea53dcb9ade0845bbca17901b48acff
Robert Krause <robert.allen.krause@gmail.com>
2023-07-22 18:09:47 -0500

    store the selected step size in EEPROM. added strings for step size

--- DDS_VFO.ino
+++ DDS_VFO.ino
@@ -2,33 +2,49 @@
 #include <RotaryEncoder.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
 
 const int W_CLK_PIN = 8;
 const int FQ_UD_PIN = 9;
 const int DATA_PIN = 10;
 const int RESET_PIN = 11;
+const int PTT_PIN = A0;
 const int ROTARY_BUTTON_PIN = A1;
+
 const long M = 1000000;
 const long K = 1000;
 double trim_freq = 125*M;
-long freq = 1.8*M;
+int ptt = 0;
+int old_ptt = 0;
+unsigned long freq = 1.8*M;
 long old_freq = 0;
 int phase = 0;
 RotaryEncoder r = RotaryEncoder(2, 3);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int rotary_button_state = 0;
 long freq_steps[] = {500, 1000, 2500, 5000};
+char* freq_step_strs[] = {"500Hz", "1kHz", "2.5kHz", "5kHz"};
 unsigned long freq_step_n = 1;
 long freq_step = freq_steps[freq_step_n];
+
+uint16_t step_addr = 0;
+uint16_t freq_addr = 1;
 
 void setup() {
   Serial.begin(9600);
   DDS.begin(W_CLK_PIN, FQ_UD_PIN, DATA_PIN, RESET_PIN);
   DDS.calibrate(trim_freq);
+  pinMode(PTT_PIN, INPUT);
   pinMode(ROTARY_BUTTON_PIN, INPUT);
   lcd.init();
   lcd.backlight();
   lcd.home();
+  // get the step from eeprom
+  uint8_t b = EEPROM.read(step_addr);
+  if (b != 0xFF) {
+    freq_step_n = b;
+    freq_step = freq_steps[freq_step_n];
+  }
   PCICR |= (1 << PCIE2);
   PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
   sei();
@@ -39,10 +55,12 @@ void setup() {
 
 void loop() {
   int state = digitalRead(ROTARY_BUTTON_PIN);
+  ptt = digitalRead(PTT_PIN);
   if (state == LOW) {
     freq_step_n = (freq_step_n + 1) % 4;
     freq_step = freq_steps[freq_step_n];
     draw_lcd();
+    EEPROM.update(step_addr, freq_step_n);
   }
   if (old_freq != freq) {
     DDS.setfreq(freq, phase);
@@ -50,26 +68,36 @@ void loop() {
     old_freq = freq;
     draw_lcd();
   }
+  if (ptt != old_ptt) {
+    old_ptt = ptt;
+    draw_lcd();
+  }
   delay(200);
 }
 
 void draw_lcd() {
-  long mhz = freq / M;
-  long decimal = freq % M;
+  unsigned long mhz = freq / M;
+  unsigned long khz = freq % M / K;
+  unsigned long hz = freq % M - khz * K;
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("VFO");
-  lcd.setCursor(7, 0);
-  lcd.print("S: ");
-  lcd.print(freq_step);
-  lcd.print("Hz");
+  lcd.print("VFO ");
+  lcd.print(ptt ? "* " : "  ");
+  lcd.print("S:");
+  lcd.print(freq_step_strs[freq_step_n]);
   lcd.setCursor(0, 1);
   lcd.print(mhz);
   lcd.print(".");
-  if (decimal) {
-    lcd.print(decimal);
+  if (khz) {
+    lcd.print(khz);
   } else {
-    lcd.print("000000");
+    lcd.print("000");
+  }
+  lcd.print(" ");
+  if (hz) {
+    lcd.print(hz);
+  } else {
+    lcd.print("000");
   }
   lcd.print(" MHz");
 }
@@ -79,10 +107,12 @@ ISR(PCINT2_vect) {
   RotaryEncoder::Direction result = r.getDirection();
   switch (result) {
   case RotaryEncoder::Direction::CLOCKWISE:
-    freq = freq - freq_step;
+    if (ptt != HIGH) 
+      freq = freq - freq_step;
     break;
   case RotaryEncoder::Direction::COUNTERCLOCKWISE:
-    freq = freq + freq_step;
+    if (ptt != HIGH)
+      freq = freq + freq_step;
     break;
   }
 }
